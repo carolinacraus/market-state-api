53f014305dc629cfa9b0df66241c934bb2774dd4
carolinacraus <carocraus@gmail.com>
2025-07-08 15:22:28 -0500

    Change debug=True in Flask app

--- app.py
+++ app.py
@@ -188,4 +188,4 @@ def _send_data_file(filename):
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
